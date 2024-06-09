48b3dd98add34bbddf8f170fc6d1afbe5c99914e
Vasishta Akinapalli <100726544+vakinapalli@users.noreply.github.com>
2024-06-08 17:41:07 -0700

    Time: 80 ms (32.15%), Space: 18.9 MB (28.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,10 +6,23 @@ class Solution(object):
                 freq[num] +=1
             else:
                 freq[num] = 1
+        ar = [[] for _ in range((len(nums) + 1))]
         ans = []
-        sorteddict = sorted(freq.items(), key = lambda item: item[1], reverse = True)
+        #sorteddict = sorted(freq.items(), key = lambda item: item[1], reverse = True)
+        for a in freq:
+            print(a, "len", len(ar))
+            
+            ar[freq[a]].append(a)
         
-        ans = [item[0] for item in sorteddict[0:k]]
+        for i in range(len(nums), 0, -1):
+            if ar[i]:
+                for elem in ar[i]:
+                    ans.append(elem)
+                    k -= 1
+                    if k == 0: 
+                        break
+                if k == 0:
+                    break
         return ans
 
             
