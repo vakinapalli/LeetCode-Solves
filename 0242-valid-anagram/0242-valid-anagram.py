8d51101c944f11eba3218873c1ad0973b2e61f2a
Vasishta Akinapalli <100726544+vakinapalli@users.noreply.github.com>
2024-09-08 18:24:49 -0400

    Time: 27 ms (82.97%), Space: 12.1 MB (63.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,24 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if len(s) != len(t):
-            return False
-
-        adic = {}
-        bdic = {}
-
-        for a,b in zip(s,t):
-            if a not in adic:
-                adic[a] = 0
-            if b not in bdic:
-                bdic[b] = 0
-            bdic[b] = bdic[b] + 1
-            adic[a] = adic[a] + 1
-        for i in adic:
-            if adic.get(i) != bdic.get(i):
-                return False
-        return True
-            
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
+        freq = {}
+        for i in s:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
         
+        for i in t:
+            if i in freq:
+                freq[i] -= 1
+            else:
+                return False
+                
+        for i in freq:
+            if freq[i] != 0:
+                return False
+        return True
