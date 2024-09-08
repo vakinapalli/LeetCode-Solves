f21f6d8a78efc96d34f3fa09b22c9981e3a52226
Vasishta Akinapalli <100726544+vakinapalli@users.noreply.github.com>
2024-09-08 19:30:08 -0400

    Time: 64 ms (89.62%), Space: 15.7 MB (68.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,14 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        dic = {}
-        for string in strs:
-            freqmap = [0 for x in range(26)]
-            for let in string:
-                freqmap[ord(let) - ord("a")] += 1
-            if tuple(freqmap) in dic:
-                dic[tuple(freqmap)].append(string)
-            else:
-                dic[tuple(freqmap)] = [string]
-        return [dic[x] for x in dic]
-
-
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
         
+        hold = {}
+        ans = []
+        for i in strs:
+            hold.setdefault("".join(sorted(i)), []).append(i)
+        
+        ans = [hold[i] for i in hold]
+        return ans
