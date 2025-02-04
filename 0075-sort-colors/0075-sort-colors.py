07b75e5c8f19c9ac6b5b4934260c893297746fe6
Vasishta Akinapalli <100726544+vakinapalli@users.noreply.github.com>
2025-02-04 18:41:52 -0500

    Time: 0 ms (100%), Space: 12.3 MB (86.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,25 +1,13 @@
 class Solution(object):
     def sortColors(self, nums):
-        r,w,b = 0,0,0
-
-        for n in nums:
-            if n == 0:
-                r += 1
-            elif n == 1:
-                w += 1
-            else:
-                b += 1
-        for i, n in enumerate(nums):
-            if r > 0:
-                nums[i] = 0
-                r -= 1
-            elif w > 0:
-                nums[i] = 1
-                w -= 1
-            else:
-                nums[i] = 2
-                b -= 1
-
+        freq = [0 for i in range(0,3)]
+        for i in nums:
+            freq[i] += 1
+        hold = 0
+        for i in range(len(freq)):
+            for j in range(0,freq[i]):
+                nums[hold] = i
+                hold += 1
         
         """
         :type nums: List[int]
