3a8d8f6168133faa010372f1d2942b67dadffe3b
Vasishta Akinapalli <100726544+vakinapalli@users.noreply.github.com>
2025-02-19 11:57:58 -0500

    Time: 253 ms (52.21%), Space: 20.2 MB (69.21%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ct = 0
+        
+        def color(row, col):
+            if row < 0 or row == len(grid) or col < 0 or col == len(grid[0]):
+                return
+            if grid[row][col] == "1":
+                grid[row][col] = "2"
+                color(row + 1, col)
+                color(row - 1, col)
+                color(row, col - 1)
+                color(row, col + 1)
+
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    print("here")
+                    color(i,j)
+                    ct += 1
+        
+            
+        
+        return ct
