a8a5e0c4df7018e070ba79ddcc4ee80fb89c4b54
Vasishta Akinapalli <100726544+vakinapalli@users.noreply.github.com>
2025-02-21 16:09:07 -0500

    Time: 0 ms (100%), Space: 17.7 MB (48.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,21 +1,10 @@
-class Solution(object):
-    def __init__(self):
-        self.hold = [0]
-    def recur(self, n):
-        if n == 1 or n == 2 or n == 3:
+class Solution:
+    dic = {}
+    def climbStairs(self, n: int) -> int:
+        if n == 1 or n == 2:
             return n
-        if self.hold[n] != 0:
-            return self.hold[n]
-        a = self.recur(n-1)
-        b = self.recur(n-2)
-        self.hold[n-1] = a
-        self.hold[n-2] = b
-        return a + b 
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        self.hold = [0] * (n + 1)
-        return self.recur(n)
+        if n in self.dic:
+            return self.dic[n]
         
+        self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dic[n]
